7e42907c0df234cca2eb9a372acac19fb32cdc47
ANGEL CHESKA <acvillalon19@gmail.com>
2024-10-03 10:27:15 +0800

    Add files via upload

---
+++ Villalon02BasicOutput.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+
+{
+	string name;
+	
+	cout << " Our Lady Of Lourdes College Foundation " << endl ;
+	cout << " College of Computer Studies " << endl  ;
+	cout << " BSIT-1B" << endl;
+	cout << " Angel Cheska S. Villaon " << endl;
+	cout << " Purok 5, Brgy. Daguit, Labo, Camarines Norte " << endl ;
+	
+	return 0;
+}
